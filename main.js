17fbf79fb86a6e6359dec0471e13565e9ddd148e
Chanwoo <tree698@gmail.com>
2022-05-09 08:59:26 +0900

    add load event

--- main.js
+++ main.js
@@ -5,25 +5,27 @@ const vertical = document.querySelector('.vertical');
 const img = document.querySelector('.img');
 const message = document.querySelector('.message');
 const coordinate = document.querySelector('.coordinate');
-const imgRect = img.getBoundingClientRect();
-
-const imgHalfWidth = imgRect.width / 2;
-const imgHalfHeight = imgRect.height / 2;
 
 const windowHalfInnerWidth = window.innerWidth / 2;
 const windowHalfInnerHeight = window.innerHeight / 2;
 
-document.addEventListener('mousemove', (event) => {
-  const X = event.clientX;
-  const Y = event.clientY;
+addEventListener('load', () => {
+  const imgRect = img.getBoundingClientRect();
+  const imgHalfWidth = imgRect.width / 2;
+  const imgHalfHeight = imgRect.height / 2;
 
-  horizontal.style.transform = `translateY(${Y - windowHalfInnerHeight}px)`;
-  vertical.style.transform = `translateX(${X - windowHalfInnerWidth}px)`;
-  img.style.transform = `translate(${
-    X - windowHalfInnerWidth - imgHalfWidth
-  }px, ${Y - windowHalfInnerHeight - imgHalfHeight}px)`;
-  message.style.transform = `translate(${X - windowHalfInnerWidth + 20}px, ${
-    Y - windowHalfInnerHeight + 20
-  }px)`;
-  coordinate.textContent = `👉    ${X}px, ${Y}px`;
+  document.addEventListener('mousemove', (event) => {
+    const X = event.clientX;
+    const Y = event.clientY;
+
+    horizontal.style.transform = `translateY(${Y - windowHalfInnerHeight}px)`;
+    vertical.style.transform = `translateX(${X - windowHalfInnerWidth}px)`;
+    img.style.transform = `translate(${
+      X - windowHalfInnerWidth - imgHalfWidth
+    }px, ${Y - windowHalfInnerHeight - imgHalfHeight}px)`;
+    message.style.transform = `translate(${X - windowHalfInnerWidth + 20}px, ${
+      Y - windowHalfInnerHeight + 20
+    }px)`;
+    coordinate.textContent = `👉    ${X}px, ${Y}px`;
+  });
 });
